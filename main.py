d5e3379f1b09b7194f5fab6b73f4f453f2b60d26
Kafando Rodrique <rdiuskaf@gmail.com>
2023-01-27 11:47:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ vcRisq = "./terms/risque.txt"
 spatial_extent = 'Montpellier'
 
 
-# Press the green button in the gutter to run the script.
+# start scrapping
 if __name__ == '__main__':
     print("scrapping...")
     scrapper(spatial_extent, vcRisq)
